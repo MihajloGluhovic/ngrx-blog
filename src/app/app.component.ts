fbed566c30c6038774d078b0029f3b4b74b9ab7c
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2024-02-27 02:34:45 +0100

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from './shared/components/topBar/topBar.component';
-import { Store } from '@ngrx/store';
-import { authActions } from './auth/store/actions';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {TopBarComponent} from './shared/components/topBar/topBar.component';
+import {Store} from '@ngrx/store';
+import {authActions} from './auth/store/actions';
 
 @Component({
   selector: 'app-root',
--- src/app/shared/components/topBar/topBar.component.ts
+++ src/app/shared/components/topBar/topBar.component.ts
@@ -1,13 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { combineLatest } from 'rxjs';
-import { selectCurrentUser } from 'src/app/auth/store/reducers';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {combineLatest} from 'rxjs';
+import {selectCurrentUser} from 'src/app/auth/store/reducers';
 
 @Component({
   selector: 'mc-topbar',
-  templateUrl: './topbar.component.html',
+  templateUrl: './topBar.component.html',
   standalone: true,
   imports: [RouterLink, CommonModule],
 })
