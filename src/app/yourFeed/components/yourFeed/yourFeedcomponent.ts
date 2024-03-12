7983c548f2d8710ce52e15e205548b38503778f1
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2024-03-12 17:38:54 +0100

    Commit

--- src/app/yourFeed/components/yourFeed/yourFeedcomponent.ts
+++ src/app/yourFeed/components/yourFeed/yourFeedcomponent.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {BannerComponent} from 'src/app/shared/components/banner/banner.component';
-import {ErrorMessageComponent} from 'src/app/shared/components/errorMessage/errorMessage.component';
 import {FeedComponent} from 'src/app/shared/components/feed/feed.component';
 import {FeedTogglerComponent} from 'src/app/shared/components/feedToggler/feedToggler.component';
 import {PopularTagsComponent} from 'src/app/shared/components/popularTags/component/popularTags.component';
